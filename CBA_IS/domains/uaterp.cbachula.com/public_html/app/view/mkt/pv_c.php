d80e1404e4d7ffb78208c9eb180e31f662961fe6
Pipp432 <pipp432@gmail.com>
2022-03-23 14:37:37 +0700

    f

--- CBA_IS/domains/uaterp.cbachula.com/public_html/app/view/mkt/pv_c.php
+++ CBA_IS/domains/uaterp.cbachula.com/public_html/app/view/mkt/pv_c.php
@@ -7,29 +7,25 @@
 
         <h2 class="mt-3">ใบชำระ supplier/PV-C</h2>
 
-      
         <div class="card shadow p-1 mt-3" style="border:none; border-radius:10px;">
             <div class="card-body">
-                <label for="UserNameTextbox">วันที่เบิก</label>
-                <input type="date" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id">
+                <label for="dateTextbox">วันที่เบิก</label>
+                <input type="date" class="form-control" id="dateTextbox" ng-change="addDate()" ng-model="withdrawDate">
                 <br>
-                <label for="UserNameTextbox">ผู้เบิกเงิน</label>
-                <input type="text" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase">
+                <label for="withdrawNameTextbox">ผู้เบิกเงิน</label>
+                <input type="text" class="form-control" id="withdrawNameTextbox" ng-change="addWithdrawName()" ng-model="withdrawName" style="text-transform:uppercase">
                 <br>
-                <label for="IDTextbox">รหัสพนักงาน</label>
-                <input type="text" class="form-control" id="IDTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase">
+                <label for="iDTextbox">รหัสพนักงาน</label>
+                <input type="text" class="form-control" id="iDTextbox" ng-change="addSeller()" ng-model="employeeId" style="text-transform:uppercase">
                 <br>
-                <label for="IDTextbox">LINE ID พนักงาน</label>
-                <input type="text" class="form-control" id="IDTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase">
+                <label for="lineIdTextbox">LINE ID พนักงาน</label>
+                <input type="text" class="form-control" id="lineIdTextbox" ng-change="addSeller()" ng-model="employeeLine" style="text-transform:uppercase">
                 <br>
-                <label for="MoneyTextbox">จำนวนเงิน</label>
-                <input type="number" class="form-control" id="MoneyTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase">    
+                <label for="taxIDTextbox">เลขที่ผู้เสียภาษีอากร</label>
+                <input type="text" class="form-control" id="taxIDTextbox" ng-change="addSeller()" ng-model="taxNumber" style="text-transform:uppercase">
                 <br>
-                <label for="TaxIDTextbox">เลขที่ผู้เสียภาษีอากร</label>
-                <input type="text" class="form-control" id="TaxIDTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase">
-                <br>
-                <label for="BankDropdown">เลขที่ผู้เสียภาษีอากร</label>
-                <select class="form-control ng-pristine ng-valid ng-empty ng-touched" ng-model="selectedProductType" id="BankDropdown" onchange='checkBank(this.value);'>
+                <label for="BankDropdown">ธนาคาร</label>
+                <select class="form-control ng-pristine ng-valid ng-empty ng-touched" ng-model="bankName" id="bankDropdown" onchange="checkBank(this.value)"; ng-change = "addBankName()">
                             <option value="" selected="selected">เลือกธนาคาร</option>
 
                             <option value="กสิกรไทย">กสิกรไทย</option>
@@ -46,15 +42,15 @@
                     <div id="otherBank" style='display:none;'>
                         <label>Enter bank name</label>
                         <br>
-                        <input type="text" class="form-control" name="color"/>
+                        <input type="text" class="form-control" ng-model="otherBankName" style="text-transform:uppercase"/>
                     </div>
                 <br>
                 <label>ชื่อบัญชีธนาคารที่รับโอน</label>
-                <input type="text" class="form-control" id="BankNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id" >
+                <input type="text" class="form-control" id="bookBankNameTextbox" ng-change="addSeller()" ng-model="bankBookName" >
                 
                 <br> 
                 <label>ผู้ร้บรอง</label>
-                <input type="text" class="form-control" id="BankNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id" >
+                <input type="text" class="form-control" id="authorizerNameTextbox" ng-change="addSeller()" ng-model="authorizerName" >
                 
                 <br> 
 
@@ -69,8 +65,8 @@
                             <td style ="border: 3px solid #dddddd;">
                                 <input type="date" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id">
                             </td>
-                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase"></td>
-                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase"></td>
+                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="detailsTextbox1" ng-change="addSeller()" ng-model="detailsTextbox1" style="text-transform:uppercase"></td>
+                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="moneyTextbox1" ng-change="addSeller()" ng-model="moneyTextbox1" style="text-transform:uppercase"></td>
                         </tr>
                         
                     </table>
@@ -88,6 +84,7 @@
         </div>
 </body>
 <script>
+    
     function checkBank(bank){
         const element = document.getElementById("otherBank");
         if(bank=='others'){
@@ -100,16 +97,46 @@
     function goToAcc(){
         window.location.href = "https://uaterp.cbachula.com/acc/confirm_payment_voucher";
     }
+    let num = 1;
     function addRows(){
-        
-        const element = document.getElementById("row1");
-        element.insertAdjacentHTML('afterend',`<tr id ="row1">
+        const element = document.getElementById(`row${num}`);
+        num++;
+        element.insertAdjacentHTML('afterend',`<tr id ="row${num}">
                             <td style ="border: 3px solid #dddddd;">
                                 <input type="date" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id">
                             </td>
-                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="UserNameTextbox" ng-change="addSeller()" ng-model="seller_employee_id" style="text-transform:uppercase"></td>
-                            <td style ="border: 3px solid #dddddd;">Germany</td>
+                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="detailsTextbox${num}" ng-change="addSeller()" ng-model="detailsTextbox${num}" style="text-transform:uppercase"></td>
+                            <td style ="border: 3px solid #dddddd;"><input type="text" class="form-control" id="moneyTextbox${num}" ng-change="addSeller()" ng-model="moneyTextbox${num}" style="text-transform:uppercase"></td>
                         </tr>`);
 
+        
+       
     }
+</script>
+<script>
+    app.controller('moduleAppController', function($scope, $http, $compile) {
+        $scope.withdrawDate='';
+        $scope.withdrawName='';
+        $scope.employeeId ='';
+        $scope.employeeLine=''
+        $scope.bankName ='';
+        $scope.taxNumber='';
+        $scope.bankBookName='';
+        $scope.authorizerName='';
+        $scope.addDate = function(){
+            console.log($scope.withdrawDate);
+
+        }
+        $scope.addWithdrawName = function(){
+            console.log($scope.withdrawName)
+
+        }
+        $scope.addBankName = function(){
+            console.log($scope.bankName)
+            window.open('/domains/uaterp.cbachula.com/public_html/app/view/file/pvc');
+
+        }
+        
+
+    })
 </script>
